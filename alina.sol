17856d6714645635a6fa1db906c671c31e9a0340
AlinaBoyeva <118597368+AlinaBoyeva@users.noreply.github.com>
2022-11-29 00:04:13 +0600

    Update alina.sol

--- alina.sol
+++ alina.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: MIT
+//SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 interface IERC20 {
@@ -28,7 +28,7 @@ contract Alina is IERC20 {
 
     mapping(address => mapping (address => uint256)) allowed;
 
-    uint256 totalSupply_ = 1000000;
+    uint256 totalSupply_ = 10000*(10**decimals) ;
 
 
    constructor() {
@@ -74,3 +74,34 @@ contract Alina is IERC20 {
 }
 
 
+contract DEX {
+
+    event Bought(uint256 amount);
+    event Sold(uint256 amount);
+
+
+    IERC20 public token;
+
+    constructor() {
+        token = new Alina();
+    }
+
+    function buy() payable public {
+        uint256 amountTobuy = msg.value;
+        uint256 dexBalance = token.balanceOf(address(this));
+        require(amountTobuy > 0, "You need to send some ether");
+        require(amountTobuy <= dexBalance, "Not enough tokens in the reserve");
+        token.transfer(msg.sender, amountTobuy);
+        emit Bought(amountTobuy);
+    }
+
+    function sell(uint256 amount) public {
+        require(amount > 0, "You need to sell at least some tokens");
+        uint256 allowance = token.allowance(msg.sender, address(this));
+        require(allowance >= amount, "Check the token allowance");
+        token.transferFrom(msg.sender, address(this), amount);
+        payable(msg.sender).transfer(amount);
+        emit Sold(amount);
+    }
+
+}
